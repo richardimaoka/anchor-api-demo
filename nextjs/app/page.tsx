9148ac698ddca9d7f27f32af95120749f2c3360f
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-24 15:47:32 +0900

    anchor positioned

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,7 +1,14 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className={styles.page}></div>
+    <div>
+      <div className={styles.page}>
+        <button className={styles.button}>
+          <div>anchor</div>
+        </button>
+        <div className={styles.positioned}>hello world</div>
+      </div>
+    </div>
   );
 }
